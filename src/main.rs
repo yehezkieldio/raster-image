f3042601986bc5315473835c11c2bdb9a7a483e1
elizielx <yehezkieldio35@gmail.com>
2024-02-25 20:52:20 +0800

    feat: load image from external url then process it

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
+use image::io::Reader as ImageReader;
+
 extern crate raster; 
 
-fn main() {
-    let image = raster::open("tests/in/Avatar.jpg");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let resp = reqwest::get("https://s4.anilist.co/file/anilistcdn/media/anime/banner/145064-esDtAY2He7sk.jpg").await?.bytes().await?;
 
-    if image.is_err() {
-        println!("Error: {:?}", image.err().unwrap());
-        return;
-    }
+    let img = ImageReader::new(std::io::Cursor::new(resp)).with_guessed_format()?.decode()?;
+    img.save("tests/in/1.jpg")?;
 
-    let mut image_real = image.unwrap();
-    raster::filter::grayscale(&mut image_real).unwrap();
+    let raster_image = raster::open("tests/in/1.jpg");
+    let mut raster_image_real = raster_image.unwrap();
+    raster::filter::grayscale(&mut raster_image_real).unwrap();
+    raster::save(&raster_image_real, "tests/out/1.jpg").unwrap();
 
-    raster::save(&image_real, "tests/out/Avatar_out.jpg").unwrap();
     print!("Test image successful.");
+
+    Ok(())
 }
